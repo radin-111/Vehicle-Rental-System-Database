5440d894403f8db7ccc7f6e2c216624988746515
radin-111 <radinsheikh87@gmail.com>
2025-12-22 18:27:06 +0600

    Adding other queries

--- queries.sql
+++ queries.sql
@@ -31,6 +31,7 @@ CREATE TABLE
         total_cost int NOT NULL
     );
 
+-- Query 1
 SELECT
     users.name,
     vehicles.name
@@ -38,3 +39,39 @@ FROM
     bookings
     INNER JOIN users ON users.user_id = bookings.user_id
     INNER JOIN vehicles ON vehicles.vehicle_id = bookings.vehicle_id;
+
+-- Query 2
+SELECT
+    *
+FROM
+    vehicles
+WHERE
+    NOT EXISTS (
+        SELECT
+            *
+        FROM
+            bookings
+        WHERE
+            bookings.vehicle_id = vehicles.vehicle_id
+    );
+
+-- Query 3
+SELECT
+    *
+FROM
+    vehicles
+WHERE
+    type = 'car';
+
+-- Query 4
+SELECT
+    vehicles.vehicle_id,
+    vehicles.name,
+    COUNT(*) AS "number of bookings"
+FROM
+    bookings
+    INNER JOIN vehicles ON bookings.vehicle_id = vehicles.vehicle_id
+GROUP BY
+    vehicles.vehicle_id
+HAVING
+    COUNT(*) > 2
